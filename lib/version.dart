438373bd854e7004b9ffa94a620f7f49f96f882e
Marco Bazzani <marco@bazzani.net>
2026-03-28 11:20:35 +0100

    Bump version to 0.2.6

--- lib/version.dart
+++ lib/version.dart
@@ -1,3 +1,3 @@
-const appVersion = '0.2.5';
+const appVersion = '0.2.6';
 const appCommit = String.fromEnvironment('COMMIT_SHA', defaultValue: 'dev');
 const appChannel = String.fromEnvironment('CHANNEL', defaultValue: 'nightly');
