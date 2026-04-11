c4c598ba565306887ae7b74d98a67b5bb2ad1129
Marco Bazzani <marco@bazzani.net>
2026-04-11 19:40:17 +0200

    Release v0.5.0

--- lib/version.dart
+++ lib/version.dart
@@ -1,4 +1,4 @@
-const appVersion = '0.4.4';
+const appVersion = '0.5.0';
 const appChannel = String.fromEnvironment('CHANNEL', defaultValue: 'nightly');
 
 /// Display version: "0.4.4" for stable releases, "0.4.4-dev" for nightly/local.
